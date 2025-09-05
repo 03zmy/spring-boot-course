372f3b61c1d47ded109e4a0b08398d3b0ceb19f1
zmy <2447186375@qq.com>
2025-09-05 10:39:52 +0800

    init:初始化父子工程学习仓库，并编写第一个接口

---
+++ spring-boot-quickstart1/src/main/java/top/zmy/boot/week1/controller/HelloController.java
@@ -0,0 +1,25 @@
+package top.zmy.boot.week1.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+/**
+ * @Author: zmy
+ * @Date: 2025/9/4
+ * @Version: 1.0
+ */
+@RestController
+public class HelloController {
+    @GetMapping("/hello")
+    public String hello()
+    {
+        return "hello world";
+    }
+    @GetMapping("/list")
+    public List<String> getList() {
+        return List.of("aaa", "bbb", "ccc");
+    }
+
+}
